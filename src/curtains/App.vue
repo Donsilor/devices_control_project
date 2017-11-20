03003084c55aea9c566eaa91bb1f07ab50789281
sheruixu <sheruixu@evergrande.cn>
2017-11-20 18:31:17 +0800

    修复窗帘动画
    Change-Id: I65658c80550f65dc63dfdf90903f80514f412ba1

--- src/curtains/App.vue
+++ src/curtains/App.vue
@@ -69,20 +69,19 @@
                 //用于确定数据是否加载完成，加载完成后渲染动态的UI
                 is_ready: false,
                 //当前窗帘打开的幅度，请注意,硬件不修改属性依旧使用close_percentage
-                //830修改为open_percentage by whx
                 open_percentage: 0,
                 //目标幅度，用于动画
                 target_percentage: 0,
                 //点击中部到指定幅度按钮显示的信息
                 tip: '',
-                //每帧运动的百分比
-                raf_percent: 0,
-                //每帧间隔时间
-                raf_time: 0,
                 //帧数动画id，用于取消
                 raf_id: 0,
+                //每帧运动的百分比
+                raf_percent: .5,
+                //每帧间隔时间
+                raf_time: 85,
                 //动画总时间，应该从服务器端获取
-                total_time: 0,
+                total_time: 4000,
                 //提示显示
                 timer: null,
                 //是否显示中部按钮提示
@@ -98,15 +97,12 @@
                 //watermark({el:'#app'})
                 //获取快照
                 HdSmart.Device.getSnapShot((data) => {
+                    HdSmart.UI.hideLoading();
+                    this.is_ready = true;
+                    this.changeRafPercent();
                     if(data && data.attribute){
-                        //4500为当前窗帘动画的总时间为hard code，后续版本需要从服务端获取
-                        this.total_time = 4500;
                         this.open_percentage = data.attribute.open_percentage;
-                        //更新每帧百分比
-                        this.changeRafPercent();
                         this.animateToTargetPercentage(this.open_percentage, true);
-                        this.is_ready = true;
-                        HdSmart.UI.hideLoading();
                     }
                 }, () => {
                     this.is_ready = true;
@@ -115,7 +111,10 @@
                 HdSmart.UI.setWebViewTouchRect(0, 0, '100%', '100%');
             });
 
+            var isFirstLoad = true
+
             HdSmart.onDeviceListen((data) => {
+                this.getTotalTime(data)
                 if (this.cbFunc) {
                     //等硬件修复了需要干掉
                     this.cbFunc();
@@ -123,18 +122,19 @@
                 }
                 try {
                     this.open_percentage = data.result.attribute.open_percentage;
-//                    this.open_percentage = data.result.attribute.close_percentage;
-                    this.animateToTargetPercentage(this.open_percentage);
+                    this.animateToTargetPercentage(this.open_percentage, isFirstLoad);
+                    isFirstLoad = false
                 } catch (error) {
 
                 }
             });
             //计算间隔帧数
-            this.getAniFramePercentage();
+            //this.getAniFramePercentage();
         },
         methods: {
+            //测RAF性能使用（受双向同步，动画的影响），第一次开发手动运行
             getAniFramePercentage() {
-                //取第20次的平均值来作为raq的间隔时间。
+                /*
                 let counter = 20;
                 let init_counter = counter;
                 let reqId = null;
@@ -163,7 +163,37 @@
                         fun(lastTime);
                     })
                 };
-                fun();
+                */
+                var start = Date.now()
+                var end
+                var count = 100
+                var time_array = []
+                var timer
+                var vm = this
+                function fun(){
+                    if(count === 0){
+                        onEnd()
+                        return
+                    }
+                    end = Date.now()
+                    if(end-start>0){
+                        time_array.push(end-start)
+                    }
+                    start = Date.now()
+                    vm.target_percentage = count--;
+                    timer = window.requestAnimationFrame(fun)
+                }
+
+                function onEnd(){
+                    var total = time_array.reduce(function(a,b){
+                        return a + b
+                    })
+                    vm.raf_time = total/time_array.length
+                    vm.changeRafPercent();
+                    window.cancelAnimationFrame(timer)
+                }
+
+                fun()
             },
 
             animateToTargetPercentage(percent, quite) {
@@ -176,6 +206,7 @@
                     this.target_percentage = percent;
                 } else {
                     let rafFunc = () => {
+
                         //下一帧数的幅度
                         let nextTargetPercentage = this.target_percentage;
                         //当前的方向
@@ -210,7 +241,8 @@
                         this.target_percentage = nextTargetPercentage;
                         this.raf_id = window.requestAnimationFrame(rafFunc)
                     };
-                    this.raf_id = window.requestAnimationFrame(rafFunc)
+                    //this.raf_id = window.requestAnimationFrame(rafFunc)
+                    rafFunc()
                 }
             },
             onOpen(onFinishCallback) {
@@ -322,6 +354,40 @@
                 if (this.raf_time && this.total_time) {
                     this.raf_percent = this.raf_time * 100 / this.total_time;
                 }
+            },
+            getTotalTime(data) {
+                if(this.isGetTotalTime){
+                    return
+                }
+                if(data.result.attribute.open_percentage===0 || data.result.attribute.open_percentage===100){
+                    return
+                }
+                if(('' + data.result.updated_at).length === 10){
+                    data.result.updated_at = Date.now()
+                }
+                this.reportTimer && clearTimeout(this.reportTimer)
+                if(!this.reportArray){
+                    this.reportArray = []
+                }
+                var len = this.reportArray.length
+                if(len === 0){
+                    this.reportArray.push(data)
+                }else{
+                    var prev = this.reportArray[len-1]
+                    if(prev.result.attribute.open_percentage !== data.result.attribute.open_percentage){
+                        this.reportArray.push(data)
+                        var speed = (data.result.attribute.open_percentage - prev.result.attribute.open_percentage)/(data.result.updated_at - prev.result.updated_at)
+                        this.total_time = Math.abs(100/speed)
+                        this.changeRafPercent()
+                        this.isGetTotalTime = true
+                        console.log('total : ', this.total_time)
+                        console.log(this.reportArray)
+                    }
+                }
+                this.reportTimer = setTimeout(()=>{
+                    this.reportArray = []
+                },1500)
+                console.log(data.result.updated_at," = ", data.result.attribute.open_percentage)
             }
         }
     }
