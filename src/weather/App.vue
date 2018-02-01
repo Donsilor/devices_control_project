88b3685a569ed93c5d9cacf15de00dc895ebdfa9
zhangruifeng <zhangruifeng@evergrande.cn>
2018-02-01 20:04:31 +0800

    完善天气预报
    Change-Id: Iad771ab9419a20c3be53a14270b0bd61b8c33d67

--- src/weather/App.vue
+++ src/weather/App.vue
@@ -1,20 +1,21 @@
 <template>
     <div id="app" :class="todyClass">
-        <div class="title">{{wList.length && wList[0].predictDate}}&nbsp;&nbsp;周一&nbsp;&nbsp;农历十月十二</div>
+        <div class="title">{{wList.length && wList[0].predictDate}}&nbsp;&nbsp;{{currentWeek}}&nbsp;&nbsp;农历{{currentOldDay}}</div>
         <div class="city">
             <div class="city-name">{{city.name}}</div>
-            <img src="./assets/icn_weather_cloudy_l.png" width="120" height="120"/>
+            <div><span class="img" :class="curMinBg"></span></div>
             <div class="city-name">{{wList.length && wList[0].tempDay}}℃&nbsp;&nbsp;{{wList.length && wList[0].conditionDay}}</div>
             <div class="city-detail">
-                <span>降水概率：10%</span>
-                <span>湿度：74%</span>
-                <span>实时空气质量：<i class="air-level">19&nbsp;优</i></span>
+                <!--<span>降水概率：10%</span>-->
+                <!--<span>湿度：74%</span>-->
+                <span>实时空气质量：<i :class="airLevel">{{curAQIvalue}}&nbsp;{{airLevelText}}</i></span>
             </div>
         </div>
         <div class="weather-list-con">
             <ul class="weather-list">
                 <li v-for="(item, index) in wList">
                     <div class="">{{index === 0 ? '今天' : item.predictDate}}</div>
+                    <div>{{item.weekday}}</div>
                     <div class="w-icon"><i :class="renderClass(item.conditionIdDay)"></i></div>
                     <div class="">{{item.tempNight}}～{{item.tempDay}}℃</div>
                     <div class="">{{item.conditionDay}}</div>
@@ -27,6 +28,9 @@
 <script>
     import jsonp from 'jsonp'
     import thisJson from './ww.json'
+    import cityIdJson from './assets/cityId.json'
+    import getOldDate from './getOldDate'
+    import remoteLoad from './loadscript'
     export default {
         name: 'app',
         data() {
@@ -35,41 +39,87 @@
                   name: '广州'
                 },
                 todyClass: '',
-                wList: []
+                wList: [],
+                curMinBg: '', //当天天气对应的大图标
+                airLevel: '', //空气质量指数
+                airLevelText: '', //空气质量描述
+                curAQIvalue: '', //实时空气质量
+                currentWeek: '', //当天为星期几
+                currentOldDay: '', //农历日期
+                allWeekDays: [
+                    {name: '周日',value: 0},
+                    {name: '周一',value: 1},
+                    {name: '周二',value: 2},
+                    {name: '周三',value: 3},
+                    {name: '周四',value: 4},
+                    {name: '周五',value: 5},
+                    {name: '周六',value: 6}
+                ]
             }
         },
         mounted() {
-            let curArr = thisJson.data['forecast']
-            this.wList = curArr.slice(0, curArr.length - 1)
-            this.renderTodayClass(this.wList[0].conditionIdDay)
-//            jsonp('http://www.sojson.com/open/api/weather/json.shtml?city=北京', null, (err, res) => {
-//                if(!err && res.errorcode === '0'){
-//                    obj.weatherCallBack(res.data)
-//                }
-//            })
+            let self = this
+            // http://www.sojson.com/open/api/weather/json.shtml?city=北京
+            let razyload = remoteLoad('http://int.dpool.sina.com.cn/iplookup/iplookup.php?format=js', false)
+            razyload.then((result) => {
+                if (remote_ip_info.ret === 1) {
+                    self.city.name = remote_ip_info.city + '市'
+                    let cityId = cityIdJson[self.city.name]
+                    self.getWeatherData(cityId)
+                }
+            })
         },
         methods: {
+            // 空气质量
+            renderAQI (val) {
+                this.curAQIvalue = val
+                if (val < 50) {
+                    this.airLevel = 'air-level-1'
+                    this.airLevelText = '优'
+                } else if (val >= 50 && val < 100) {
+                    this.airLevel = 'air-level-2'
+                    this.airLevelText = '良'
+                } else if (val >= 100 && val < 150) {
+                    this.airLevel = 'air-level-3'
+                    this.airLevelText = '轻度污染'
+                } else if (val >= 150 && val < 200) {
+                    this.airLevel = 'air-level-4'
+                    this.airLevelText = '中度污染'
+                } else if (val >= 200 && val < 300) {
+                    this.airLevel = 'air-level-5'
+                    this.airLevelText = '重度污染'
+                } else {
+                    this.airLevel = 'air-level-6'
+                    this.airLevelText = '严重污染'
+                }
+            },
+            // 渲染背景图片(对应阴,晴,下雨..等)
             renderTodayClass (val) {
                 switch (val) {
                     case '0':
                         this.todyClass = 'img_bg_weather_sunny'
+                        this.curMinBg = 'icn_weather_sunny'
                         break
                     case '1':
                     case '2':
                         this.todyClass = 'img_bg_weather_cloudy'
+                        this.curMinBg = 'icn_weather_cloudy'
                         break
                     case '3':
                     case '7':
                     case '9':
                     case '10':
                         this.todyClass = 'img_bg_weather_rainy'
+                        this.curMinBg = 'icn_weather_rainy'
                         break
                     case '4':
                         this.todyClass = 'img_bg_weather_thuner'
+                        this.curMinBg = 'icn_weather_thuner'
                         break
                     case '20':
                     case '29':
                         this.todyClass = 'img_bg_weather_wind'
+                        this.curMinBg = 'icn_weather_wind'
                         break
                     case '13':
                     case '14':
@@ -77,11 +127,13 @@
                     case '16':
                     case '17':
                         this.todyClass = 'img_bg_weather_snowy'
+                        this.curMinBg = 'icn_weather_snowy'
                         break
                     default:
                         break
                 }
             },
+            // 渲染6天天气对应的图标图像
             renderClass (val) {
                 let curClass = ''
                 switch(val) {
@@ -122,6 +174,48 @@
                         break
                 }
                 return curClass
+            },
+            getWeatherData (id,) {
+                let thisDay = (new Date()).getDay()
+                let curIndex = 0
+                let currentWeekArr = []
+                let awds = this.allWeekDays
+                awds.forEach((item, index) => {
+                    if (item.value === thisDay) {
+                        curIndex = index
+                        this.currentWeek = awds[index].name
+                    }
+                })
+                for (let i = 0; i < 7; i++) {
+                    if (!awds[curIndex]) {
+                        curIndex = 0
+                    }
+                    currentWeekArr.push(awds[curIndex].name)
+                    curIndex ++
+                }
+                this.currentOldDay = getOldDate()  //获取农历日期
+                let self = this
+                HdSmart.ready(() => {
+                    HdSmart.Device.control({
+                        method: '3d_get_moji_weather',
+                        params: {
+                            cityId: '2'
+                        }
+                    }, (res) => {
+                        let innerData = res.result
+                        self.renderAQI(innerData.aqi.data['aqi'].value)
+                        let forecast = innerData.forecast.data['forecast']
+                        let newArr = forecast.slice(0, forecast.length - 1)
+                        self.wList = newArr.map((item, index) => {
+                            item.weekday = currentWeekArr[index]
+                            return item
+                        })
+                        self.renderTodayClass(this.wList[0].conditionIdDay)
+                    }, (err) => {
+                        console.log('c错误了。。。。。。。。。。')
+                        console.log(err)
+                    })
+                })
             }
         }
     }
@@ -203,12 +297,54 @@
         }
         .city-detail{
             font-size: 30px;
-            .air-level{
-                background: #32cb72;
+            i{
                 border-radius: 6px;
                 font-style: normal;
+                color: #333;
                 padding: 5px 10px;
             }
+            .air-level-1{
+                background: #07FD04;
+            }
+            .air-level-2{
+                background: #FFFF00;
+            }
+            .air-level-3{
+                background: #FF7805;
+            }
+            .air-level-4{
+                background: #FA0100;
+            }
+            .air-level-5{
+                background: #9E004F;
+            }
+            .air-level-6{
+                background: #790222;
+            }
+        }
+        .img{
+            display: inline-block;
+            width: 120px;
+            height: 120px;
+            background-size: 100% 100%;
+        }
+        .icn_weather_sunny{
+            background-image: url('./assets/icn_weather_sunny_l.png');
+        }
+        .icn_weather_cloudy{
+            background-image: url('./assets/icn_weather_cloudy_l.png');
+        }
+        .img_bg_weather_rainy{
+            background-image: url('./assets/icn_weather_rainy_l.png');
+        }
+        .icn_weather_thuner{
+            background-image: url('./assets/icn_weather_thunder_l.png');
+        }
+        .icn_weather_wind{
+            background-image: url('./assets/icn_weather_wind_l.png');
+        }
+        .icn_weather_snowy{
+            background-image: url('./assets/icn_weather_snow_l.png');
         }
     }
     .weather-list-con{
@@ -219,7 +355,7 @@
             li{
                 list-style: none;
                 .w-icon{
-                    padding:50px 0;
+                    padding:40px 0;
                     i{
                         display: inline-block;
                         height: 60px;
