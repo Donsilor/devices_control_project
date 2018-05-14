297c6a33ea969916a40ba900572aaba770127e72
guo <guoxian@evergrande.cn>
2018-05-14 09:14:16 +0800

    窗帘宽度样式问题修复
    Change-Id: I7e5831e5fb0299cb600dc7a754600a783d0cdf55

--- src/curtains_m/components/Curtain.vue
+++ src/curtains_m/components/Curtain.vue
@@ -16,103 +16,101 @@
 </template>
 
 <style scoped lang="less">
-    .wrap {
-        width: 622px;
-        height: 400px;
-        position: relative;
-        visibility: hidden;
-        overflow: hidden;
-    }
+.wrap {
+    width: 622px;
+    height: 400px;
+    position: relative;
+    visibility: hidden;
+    overflow: hidden;
+}
 
-    .wrap.visible {
-        visibility: visible;
-    }
+.wrap.visible {
+    visibility: visible;
+}
 
-    .bg {
-        background: url('../assets/img_curtain_view@2x.png') center center no-repeat;
-        width: 591px;
-        height: 376px;
-        background-size: 100% 100%;
-        position: absolute;
-        bottom: 0;
-        left: 50%;
-        transform: translate3d(-50%, 0, 0);
-    }
+.bg {
+    background: url("../assets/img_curtain_view@2x.png") center center no-repeat;
+    width: 591px;
+    height: 376px;
+    background-size: 100% 100%;
+    position: absolute;
+    bottom: 0;
+    left: 50%;
+    transform: translate3d(-50%, 0, 0);
+}
 
-    .left {
-        position: absolute;
-        left: 0;
-        top: 0;
-        transform-origin: 0 0;
-    }
+.left {
+    position: absolute;
+    left: 0;
+    top: 0;
+    transform-origin: 0 0;
+}
 
-    .right {
-        position: absolute;
-        right: 0;
-        top: 0;
-        transform-origin: 100% 0;
-    }
+.right {
+    position: absolute;
+    right: 0;
+    top: 0;
+    transform-origin: 100% 0;
+}
 
-    .box {
-        width: 310px;
-        height: 400px;
-        position: absolute;
-    }
+.box {
+    width: 49.61%;
+    height: 400px;
+    position: absolute;
+}
 
-    .clothes {
-        background: url('../assets/img_curtain_clothes@2x.png') center center no-repeat;
-        width: 100%;
-        height: 100%;
-        background-size: 100% 100%;
-    }
+.clothes {
+    background: url("../assets/img_curtain_clothes@2x.png") center center
+        no-repeat;
+    width: 100%;
+    height: 100%;
+    background-size: 100% 100%;
+}
 
-    .folding {
-        background: url('../assets/img_curtain_folding@2x.png') center center no-repeat;
-        width: 100%;
-        height: 100%;
-        background-size: 100% 100%;
-        // position: absolute;
-        // top: 0;
-    }
-    .left > div{
-        left: 0;
-    }
-    .right > div{
-        right: 0;
-        float: right;
-    }
-    .test{
-        height: 1px;
-        background: none;
-    }
+.folding {
+    background: url("../assets/img_curtain_folding@2x.png") center center
+        no-repeat;
+    width: 100%;
+    height: 100%;
+    background-size: 100% 100%;
+    // position: absolute;
+    // top: 0;
+}
+.left > div {
+    left: 0;
+}
+.right > div {
+    right: 0;
+    float: right;
+}
+.test {
+    height: 1px;
+    background: none;
+}
 </style>
 <script>
- const BGMOVEDISTANCE = 24;
-    export default {
-        props: {
-            open_percentage: Number,
-            is_ready: Boolean,
-            test: Number
+const BGMOVEDISTANCE = 24;
+export default {
+    props: {
+        open_percentage: Number,
+        is_ready: Boolean
+    },
+    computed: {
+        //背景图移动的距离
+        moveX() {
+            return BGMOVEDISTANCE * this.open_percentage / 100;
         },
-        computed: {
-            //背景图移动的距离
-            moveX (){
-                return BGMOVEDISTANCE * this.open_percentage / 100
-            },
-            //云背景的透明度
-            bg_opacity (){
-                return 0.7 + 0.3 * this.open_percentage / 100;
-            },
-            //窗帘黑色背景图的透明度
-            opacity (){
-                return 0.1 + 0.9 * this.open_percentage / 100;
-            },
-            width (){
-                return (100 - 0.8 * this.open_percentage) + '%'
-            },
-            w1 (){
-                return (100 - 0.8 * this.test) + '%'
-            }
+        //云背景的透明度
+        bg_opacity() {
+            return 0.7 + 0.3 * this.open_percentage / 100;
+        },
+        //窗帘黑色背景图的透明度
+        opacity() {
+            return 0.1 + 0.9 * this.open_percentage / 100;
+        },
+        width() {
+            return 100 - 0.8 * this.open_percentage + "%";
         }
     }
+};
 </script>
