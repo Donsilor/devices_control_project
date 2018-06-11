10529d942b7222422c9001af55b4a8f158e86ce4
sheruixu <sheruixu@evergrande.cn>
2018-06-11 09:07:29 +0800

    up tv
    Change-Id: If8948dee56980f30d7aa295b62d4f533c3a54023

--- src/tv/main.js
+++ src/tv/main.js
@@ -51,7 +51,7 @@ const store = new Vuex.Store({
         activeDetail: {},
         device_name: '',
         tvStatus: {
-            tvOnlineStatus: -1,
+            tvOnlineStatus: 1,
             screenProjectType: 0,
             screenProjectTitle: ''
         }
