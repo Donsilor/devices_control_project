567b5266b5e44628c321f289f08b11c12e4a6e63
lily <zhangi1@hdsc.com>
2019-02-27 10:23:37 +0800

    融合版空调皮肤修改

--- src/tv_m/main.js
+++ src/tv_m/main.js
@@ -21,8 +21,8 @@ import ErrorView from "./components/Error.vue";
 import OfflineMask from "./components/OfflineMask.vue";
 //import Statusbar from './components/Statusbar.vue'
 import StatusTip from "./components/StatusTip.vue";
-import AlloyFinger from 'alloyfinger/alloy_finger'
-import AlloyFingerVue from 'alloyfinger/vue/alloy_finger.vue'
+import AlloyFinger from 'alloyfinger'
+//import AlloyFingerVue from 'alloyfinger/vue/alloy_finger.vue'
 
 import * as service from "./service";
 
@@ -33,9 +33,10 @@ Vue.use(VueLazyload, {
     preLoad: 1.3,
     attempt: 1
 });
-Vue.use(AlloyFingerVue, {
-    AlloyFinger
-})
+// Vue.use(AlloyFingerVue, {
+//     AlloyFinger
+// })
+Vue.use(AlloyFinger)
 
 Vue.component("detail", Detail);
 Vue.component("topbar", Topbar);
