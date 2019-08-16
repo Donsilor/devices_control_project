76e9ea6781940d1bd393ea9e03ae8df573dec986
liuxionghui <liuxionghui@evergrande.com>
2019-08-16 16:26:55 +0800

    空气检测仪完成

--- src/broadLink/air_quality_check_m/components/Index.vue
+++ src/broadLink/air_quality_check_m/components/Index.vue
@@ -10,38 +10,38 @@
           @click="PMsynopsis()" />
         <div 
           class="PM PM-close">
-          <span class="num">26</span>
+          <span class="num">{{ computePM25 }}</span>
           <span class="unit">ug/m3</span>
         </div>
         <p 
-          class="describe">PM2.5 优</p>
+          class="describe">PM2.5 {{ computePM25Text }}</p>
       </div>
 
 
       <div class="information first">
         <p>
-          <span>  <i class="num">26</i> °C</span>
-          <span class="text">温度 凉爽</span> 
+          <span>  <i class="num">{{ computeTemperature }}</i> <i class="unit">°C</i>  </span>
+          <span class="text">温度 {{ computeTemperatureText }}</span> 
         </p>
         <p> 
-          <span>  <i class="num">26</i> °C</span>
-          <span class="text">温度 凉爽</span> 
+          <span>  <i class="num">{{ computeHumidity }}</i> <i class="unit">%</i></span>
+          <span class="text">湿度 {{ computeHumidityText }}</span> 
         </p>
         <p>
-          <span>  <i class="num">26</i> °C</span>
-          <span class="text">温度 凉爽</span> 
+          <span>  <i class="num">--</i> <i class="unit">lux</i></span>
+          <span class="text">光照度</span> 
         </p>      
       </div>
 
 
-      <div class="information">
+      <div class="information flex">
         <p>
-          <span>  <i class="num">26</i> °C</span>
-          <span class="text">温度 凉爽</span> 
+          <span>  <i class="num">{{ computeTVOC }}</i> <i class="unit">mg/m3</i></span>
+          <span class="text">TVOC {{ computeTVOCText }}</span> 
         </p>
         <p class="last">
-          <span><i class="num">5350</i> °C</span>
-          <span class="text">温度 凉爽</span> 
+          <span><i class="num">{{ deviceAttrs.co2||'----' }}</i> <i class="unit">ppm</i></span>
+          <span class="text">CO2a {{ computeCO2Text }}</span> 
         </p>
 
       </div>
@@ -49,34 +49,169 @@
   </div>
 </template>
 <script>
-import mapActions from 'vuex'
+import {mapActions,mapState} from 'vuex'
 export default {
   data(){
     return {
 
     }
   },
- 
-
   computed:{
+    ...mapState(['device', 'deviceAttrs']),
     rotateClass(){
-      // eslint-disable-next-line no-constant-condition
-      if(1 > 2){
-        return "aaa"
+      if(this.deviceAttrs['pm2.5']&&this.deviceAttrs['pm2.5']>0){
+        if(this.deviceAttrs['pm2.5']>0&&this.deviceAttrs['pm2.5']<=350){
+          return 'lvs2'
+        }else if(this.deviceAttrs['pm2.5']>350&&this.deviceAttrs['pm2.5']<=750){
+          return 'huangs'
+        }else if(this.deviceAttrs['pm2.5']>750&&this.deviceAttrs['pm2.5']<=1150){
+          return 'huangs'
+        }else if(this.deviceAttrs['pm2.5']>1150&&this.deviceAttrs['pm2.5']<=1500){
+          return 'hongse'
+        }else if(this.deviceAttrs['pm2.5']>1500&&this.deviceAttrs['pm2.5']<=2550){
+          return 'shenghong'
+        }else{
+          return 'shenheng'
+        }
       }else{
-        return 'bbb'
+        return '----'
+      }
+    },
+    computePM25(){
+      if(this.deviceAttrs['pm2.5']){
+        return  this.deviceAttrs['pm2.5']/10
+      }else{
+        return '---'
+      }
+    },
+    computePM25Text(){
+      if(this.deviceAttrs['pm2.5']&&this.deviceAttrs['pm2.5']>0){
+        if(this.deviceAttrs['pm2.5']>0&&this.deviceAttrs['pm2.5']<=350){
+          return '优'
+        }else if(this.deviceAttrs['pm2.5']>350&&this.deviceAttrs['pm2.5']<=750){
+          return '良'
+        }else if(this.deviceAttrs['pm2.5']>750&&this.deviceAttrs['pm2.5']<=1150){
+          return '轻度污染'
+        }else if(this.deviceAttrs['pm2.5']>1150&&this.deviceAttrs['pm2.5']<=1500){
+          return '中度污染'
+        }else if(this.deviceAttrs['pm2.5']>1500&&this.deviceAttrs['pm2.5']<=2550){
+          return '重度污染'
+        }else{
+          return '严重污染'
+        }
+      }else{
+        return '----'
+      }
+      
+    },
+    computeTemperature(){
+      if(this.deviceAttrs.temperature){
+        return  this.deviceAttrs.temperature/10
+      }else{
+        return '---'
+      }
+    },
+    computeTemperatureText(){
+      if(this.deviceAttrs.temperature){
+        if(this.deviceAttrs.temperature<=-100){
+          return '极寒'
+        }else if(this.deviceAttrs.temperature>-100&&this.deviceAttrs.temperature<=100){
+          return '寒冷'
+        }else if(this.deviceAttrs.temperature>100&&this.deviceAttrs.temperature<=150){
+          return '较冷'
+        }else if(this.deviceAttrs.temperature>150&&this.deviceAttrs.temperature<=220){
+          return '凉爽'
+        }else if(this.deviceAttrs.temperature>220&&this.deviceAttrs.temperature<=260){
+          return '适宜'
+        }else if(this.deviceAttrs.temperature>260&&this.deviceAttrs.temperature<=300){
+          return '较热'
+        }else if(this.deviceAttrs.temperature>300&&this.deviceAttrs.temperature<=340){
+          return '炎热'
+        }else{
+          return '酷热'
+        }
+      }else{
+        return '--'
+      }
+    },
+    computeHumidity(){
+      if(this.deviceAttrs.humidity){
+        return  this.deviceAttrs.humidity/100
+      }else{
+        return '---'
+      }
+    },
+    computeHumidityText(){
+      if(this.deviceAttrs.humidity){
+        if(this.deviceAttrs.humidity<=1000){
+          return '极度干燥'
+        }else if(this.deviceAttrs.humidity>1000&&this.deviceAttrs.humidity<=3000){
+          return '较为干燥'
+        }else if((this.deviceAttrs.humidity>3000&&this.deviceAttrs.humidity<=4000)||(this.deviceAttrs.humidity>6000&&this.deviceAttrs.humidity<=7000)){
+          return '较适宜'
+        }else if(this.deviceAttrs.humidity>4000&&this.deviceAttrs.humidity<=6000){
+          return '适宜'
+        }else if(this.deviceAttrs.humidity>7000&&this.deviceAttrs.humidity<=9000){
+          return '较潮湿'
+        }else{
+          return '非常潮湿'
+        }
+      }else{
+        return '----'
+      }
+    },
+    computeTVOC(){
+      if(this.deviceAttrs.tvoc){
+        return  this.deviceAttrs.tvoc/10
+      }else{
+        return '---'
+      }
+    },
+    computeTVOCText(){
+      if(this.deviceAttrs.tvoc&&this.deviceAttrs.tvoc>0){
+        if(this.deviceAttrs.tvoc>0&&this.deviceAttrs.tvoc<=4){
+          return '优'
+        }else if(this.deviceAttrs.tvoc>4&&this.deviceAttrs.tvoc<=6){
+          return '良'
+        }else if(this.deviceAttrs.tvoc>6&&this.deviceAttrs.tvoc<=30){
+          return '轻度污染'
+        }else if(this.deviceAttrs.tvoc>30&&this.deviceAttrs.tvoc<=100){
+          return '中度污染'
+        }else if(this.deviceAttrs.tvoc>100&&this.deviceAttrs.tvoc<=250){
+          return '重度污染'
+        }else{
+          return '严重污染'
+        }
+      }else{
+        return '----'
+      }
+    },
+    computeCO2Text(){
+      if(this.deviceAttrs.co2&&this.deviceAttrs.co2>350){
+        if(this.deviceAttrs.co2>350&&this.deviceAttrs.co2<=450){
+          return '正常'
+        }else if(this.deviceAttrs.co2>450&&this.deviceAttrs.co2<=1000){
+          return '较正常'
+        }else if(this.deviceAttrs.co2>1000&&this.deviceAttrs.co2<=2000){
+          return '较浑浊'
+        }else if(this.deviceAttrs.co2>2000&&this.deviceAttrs.co2<=5000){
+          return '浑浊'
+        }else{
+          return '过于浑浊'
+        }
+      }else{
+        return '----'
       }
     }
   },
   created() {
     HdSmart.ready(() => {
-      // this.getDeviceInfo()
+      this.getDeviceInfo()
     })
   },
   methods: {
-    // ...mapActions(['getDeviceInfo']),
+    ...mapActions(['getDeviceInfo']),
     PMsynopsis(){
-      console.log(this.deviceAttrs)
       this.$router.push('/PMsynopsis')
     }
   }
@@ -86,14 +221,6 @@ export default {
 <style lang="less" scoped>
 @lib: "../../../../lib/base/haier_hood/assets";
 @lib1: "../../../lib";
-// @keyframes rotate {
-//     from {
-//       transform:rotate(0deg);
-//     }
-//     to {
-//       transform:rotate(360deg);
-//     }
-// }
 i{
    font-style:normal
 }
@@ -110,44 +237,36 @@ i{
       height: 524px;
       flex-direction: column;
       color: #20282B;
-      .aaa {
-        background: url("@{lib}/yanji_img_animation.png") no-repeat;
+       .lvs2 {
+        background: url("~@lib/base/air_quality_check/assets/lvs2.png") no-repeat;
         background-size: 100% 100%;
-        &.rotate-low{
-          animation: rotate 10s linear 0s infinite;
         }
-        &.rotate-overlow{
-          animation: rotate 8s linear 0s infinite;
-        }
-        &.rotate-normal{
-          animation: rotate 6s linear 0s infinite;
-        }
-        &.rotate-overnormal{
-          animation: rotate 4s linear 0s infinite;
-        }
-        &.rotate-high{
-          animation: rotate 2s linear 0s infinite;
-        }
-      }
-      .bbb{
-        background: url("@{lib}/yanji_img_animation.png") no-repeat;
+      .huangs {
+        background: url("~@lib/base/air_quality_check/assets/huangs.png") no-repeat;
         background-size: 100% 100%;
-        &.rotate-low{
-          animation: rotate 10s linear 0s infinite;
         }
-        &.rotate-overlow{
-          animation: rotate 8s linear 0s infinite;
+      
+      .huangs {
+        background: url("~@lib/base/air_quality_check/assets/huangs.png") no-repeat;
+        background-size: 100% 100%;
         }
-        &.rotate-normal{
-          animation: rotate 6s linear 0s infinite;
+      
+      .hongse {
+        background: url("~@lib/base/air_quality_check/assets/hongse.png") no-repeat;
+        background-size: 100% 100%;
         }
-        &.rotate-overnormal{
-          animation: rotate 4s linear 0s infinite;
+      
+      .shenghong {
+        background: url("~@lib/base/air_quality_check/assets/shenghong.png") no-repeat;
+        background-size: 100% 100%;
         }
-        &.rotate-high{
-          animation: rotate 2s linear 0s infinite;
+      
+      .shenheng {
+        background: url("~@lib/base/air_quality_check/assets/shenheng.png") no-repeat;
+        background-size: 100% 100%;
         }
-      }
+    }
+      
       .bg{
         position: absolute;
         top: 0;
@@ -185,13 +304,13 @@ i{
       &.first{
         margin-top:60px;
         margin-bottom: 60px;
-        border-bottom: 1px #000 solid;
+        border-bottom: 1px #0000004f solid;
         padding-bottom: 60px;
       }
       p{
         padding: 0 40px;
-        border-left: 1px #000 solid;
-         flex: 1;
+        border-left: 1px #0000004f solid;
+        //  flex: 1;
         &:nth-child(1){
           border: 0
         }
@@ -202,12 +321,22 @@ i{
           display: block
         }
         .num{
-          font-size: 72px;
+          font-size: 62px;
+        }
+        .unit{
+          font-size: 24px;
+          position: relative;
+          top:-40px;
         }
         .text{
           font-size: 24px;
         }
       }
     }
-  }
+    .flex{
+      p{
+        flex: 1
+      }
+    }
+  
 </style>
