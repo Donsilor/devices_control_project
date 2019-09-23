9d0b871755effb9a7132236f1e60471a9872e1dd
liuxionghui <liuxionghui@evergrande.com>
2019-09-23 09:34:05 +0800

    灯泡联调

--- src/hongyan/hongyan_light_m/App.vue
+++ src/hongyan/hongyan_light_m/App.vue
@@ -1,45 +1,51 @@
 <template class="a">
   <div class="body">
+    
     <div :class="[{ 'offline': isOffline }, {'close': isClose}, 'page']">
       <topbar 
         :show-right="false"
         :title="device.device_name"
         bak-color="#fff" />
-      <!-- :title="device.device_name" -->
       <div
         v-if="toogleSpeed" 
-        class="main center" >
+        class="main center" 
+      >
+        <div 
+          v-show="!isClose&&!isOffline" 
+          class="ratiobg111"
+          @click="ratiobg2click($event)">
+          <div 
+            class="ratiobg2" 
+          />
+          <div 
+            class="circle" 
+            @touchstart="touchStart($event)"
+            @touchmove="touchMove($event)"
+            @touchend="touchEnd($event)"/>
+            <!-- <div class="an">暗</div>
+          <div class="liang">亮</div> -->
+        </div>
         <div class="wrap-circle center">
-          <div class="lianggang"/>
+          <!-- <div class="lianggang"/> -->
           <div 
             :class="[{'animation': !isClose }, {'greycircle': isClose }, rotateClass, 'bg']" />
-            <!-- <p class="wind">风速档位</p>
-          <p 
-            v-if="!isClose&&!isOffline"
-            class="speed">{{ speedNum }}</p> -->
-            <!-- <div 
-            v-else 
-            class="speed speed-close">
-            <span/>
-            <span/>
-          </div> -->
-            <!-- <p 
-            v-if="!isClose&&!isOffline"
-            class="switch">{{ speedText }}</p> --> 
         </div>
       </div>
       <div 
         v-show="!isClose&&!isOffline"
         class="tips">
-        <span>亮度 {{ deviceAttrs.level }}%</span>
+        <span>亮度 {{ ratio }}%</span>
+        <!-- <div class="ratiobg">
+          <div class="ratiobg2"/>
+          <div 
+            class="circle" 
+            @touchstart="touchStart($event)"
+            @touchmove="touchMove($event)"
+            @touchend="touchEnd($event)"/>
+          <div class="an">暗</div>
+          <div class="liang">亮</div>
+        </div> -->
       </div>
-      <!-- <div 
-        v-show="deviceAttrs.delay == 'on'&&deviceAttrs.switch=='on'"
-        class="tips tips2">
-        <i />
-        <span>延迟关机中</span>
-      </div> -->
-
       <!-- 按钮 -->
       <div class="panel-btn center">
         <div :class="[{'up-index': !isOffline }, 'btn-wrap']">
@@ -50,20 +56,20 @@
         </div>
         <div 
           class="btn-wrap"
-          @click="setSpeed(3000)">
-          <div :class="[{ 'active': deviceAttrs.temperature == 3000 }, 'btn btn-rs center']" />
+          @click="setSpeed(370)">
+          <div :class="[{ 'active': deviceAttrs.temperature == 370 }, 'btn btn-rs center']" />
           <div class="btn-name">暖光</div>
         </div>
         <div 
           class="btn-wrap"
-          @click="setSpeed(5000)">
-          <div :class="[ { 'active': deviceAttrs.temperature == 5000 }, 'btn btn-gs center']" />
+          @click="setSpeed(240)">
+          <div :class="[ { 'active': deviceAttrs.temperature == 240 }, 'btn btn-gs center']" />
           <div class="btn-name">自然光</div>
         </div>
         <div 
           class="btn-wrap"
-          @click="setSpeed(6500)">
-          <div :class="[ { 'active': deviceAttrs.temperature == 6500 }, 'btn btn-bc center']" />
+          @click="setSpeed(167)">
+          <div :class="[ { 'active': deviceAttrs.temperature == 167}, 'btn btn-bc center']" />
           <div class="btn-name">白光</div>
         </div>
       </div>
@@ -81,9 +87,8 @@ export default {
       speedText: '关',
       time:0,
       dateObj:null,
-      // minute: '--',
-      // second: '--',
-      toogleSpeed:true
+      toogleSpeed:true,
+      ratio:0
     }
   },
   computed: {
@@ -114,24 +119,19 @@ export default {
           return ''
       }
     },
+    isClose(){
+      return this.deviceAttrs.switch_status=="on"?false:true
+    }
   },
   watch: {
-    'device.stateChange'() {
-      this.$nextTick(() => {
-        this.toogleSpeed = false
-        this.$nextTick(() => {
-          this.toogleSpeed = true
-        })
-      })
-      // if(this.deviceAttrs.delay == 'on'){
-      //   this.time = 3*60
-      //   this.countdown()
-      //   clearInterval(this.dateObj)
-      //   this.dateObj = setInterval(this.countdown, 1000)
-      // }else{
-      //    clearInterval(this.dateObj)
-      // }     
-    },
+    // 'device.stateChange'() {
+    //   this.$nextTick(() => {
+    //     this.toogleSpeed = false
+    //     this.$nextTick(() => {
+    //       this.toogleSpeed = true
+    //     })
+    //   })   
+    // },
     deviceAttrs: {
       handler(newName) {
         switch (newName.temperature) {
@@ -161,7 +161,17 @@ export default {
   created() {
     HdSmart.ready(() => {
       this.getDeviceInfo()
+      .then(()=>{
+        this.ratio = parseInt(this.deviceAttrs.level/254*100) 
+        let circle = document.querySelectorAll(".circle")[0]  
+        let ratiobg2 = document.querySelectorAll(".ratiobg2")[0]  
+        circle.style.top =  this.ratio/100*250 +"px"
+        ratiobg2.style.height =  this.ratio/100*250 +"px"
+      })
     })
+  },
+  mounted(){
+    
   },
   methods: {
     ...mapActions(['getDeviceInfo', 'doControlDevice']),
@@ -174,13 +184,79 @@ export default {
     },
     setSwitch() {
       let switchStatus = ''
-      if (this.deviceAttrs.switch == 'on') {
+      if (this.deviceAttrs.switch_status == 'on') {
         switchStatus = 'off'
       } else {
         switchStatus = 'on'
       }
       this.controlDevice("switch", switchStatus)
     }, 
+    // touchStart(e){
+    //   e.stopPropagation()
+    //   e.preventDefault()
+    //   console.log(e)
+    // },
+    // touchMove(e){
+    //   e.stopPropagation()
+    //   e.preventDefault()
+    //   let ratiobg = document.querySelectorAll(".ratiobg")[0]
+    //   let circle = document.querySelectorAll(".circle")[0]  
+    //   let ratiobg2 = document.querySelectorAll(".ratiobg2")[0]  
+    //   let w = e.targetTouches[0].pageX - ratiobg.offsetLeft
+    //   w=w<=0?0:w
+    //   w=w>=300?300:w
+    //   circle.style.left =  w +"px"
+    //   ratiobg2.style.width = w +"px"
+    //   this.ratio = parseInt((w/300)*100)
+    //   // console.log(ratio)
+    // },
+    // touchEnd(e){
+    //   e.stopPropagation()
+    //   e.preventDefault()
+    //   let level = parseInt(this.ratio/100*254) 
+    //   this.controlDevice('level', level)
+    // },
+    touchStart(e){
+      console.log(e)
+    },
+    touchMove(e){
+      e.stopPropagation()
+      e.preventDefault()
+      let ratiobg = document.querySelectorAll(".ratiobg111")[0]
+      let circle = document.querySelectorAll(".ratiobg111>.circle")[0]  
+      let ratiobg2 = document.querySelectorAll(".ratiobg111>.ratiobg2")[0]  
+      // let main = document.querySelectorAll(".main")[0]
+      // console.log(main.offsetTop)
+      // console.log( ratiobg.offsetTop)
+    
+      let h = e.targetTouches[0].pageY - ratiobg.offsetTop
+      h=h<=0?0:h
+      h=h>=250?250:h
+      circle.style.top = h + ratiobg2.offsetTop +"px"
+      ratiobg2.style.height = h +"px"
+     
+      // circle.style.left =  w +"px"
+      // ratiobg2.style.width = w +"px"
+      this.ratio = parseInt((h/250)*100)
+    },
+    touchEnd(e){
+      e.stopPropagation()
+      e.preventDefault()
+      let level = parseInt(this.ratio/100*254) 
+      this.controlDevice('level', level)
+    },
+    ratiobg2click(e){
+      e.stopPropagation()
+      e.preventDefault()
+      let circle = document.querySelectorAll(".ratiobg111>.circle")[0]  
+      let ratiobg2 = document.querySelectorAll(".ratiobg111>.ratiobg2")[0] 
+      circle.style.top = e.offsetY +"px"
+      ratiobg2.style.height = e.offsetY +"px"
+      this.ratio = parseInt(( e.offsetY /250)*100)
+      let level = parseInt(this.ratio/100*254) 
+      this.controlDevice('level', level)
+    }
+    , 
     // countdown(){
     //       this.second = Math.floor(this.time  % 60)
     //       this.minute = Math.floor(this.time /  60 % 60)
@@ -191,14 +267,28 @@ export default {
     //       this.time--
     // },
     controlDevice(attr, value) {
-      return this.doControlDevice({
-        nodeid: `haier_hood.main.${attr}`,
+      if(attr=='switch'){
+         return this.doControlDevice({
+        nodeid: `bulb.main.${attr}`,
         params: {
           attribute: {
             [attr]: value
           }
         }
       })
+      }else{
+        return this.doControlDevice({
+        nodeid: `bulb.main.${attr}`,
+        params: {
+          attribute: {
+            [attr]: value,
+            "transition_time":2,
+            "need_confirm":true
+          }
+        }
+      })
+      }
+      
     },
     handleMore() {
       this.isOpen = !this.isOpen
@@ -265,10 +355,49 @@ export default {
   }
 }
  .tips{
-    margin-top: 40px;
+   position: relative;
+    margin-top: 100px;
     text-align: center;
-    font-size: 12px;
+    font-size: 36px;
     color: #fff;
+    display: flex;
+    justify-content: center;
+    .ratiobg{
+      position: absolute;
+      width: 600px;
+      height: 10px;
+      background: #ccc;
+      top: 160px;
+      // left: 50%;
+      // transform: translateX(-50%);
+      border-radius: 60px;
+      .ratiobg2{
+        width: 0px;
+        height: 100%;
+        background:aqua;
+        border-radius: 60px;
+      }
+      .circle{
+        width: 40px;
+        height: 40px;
+        border-radius: 50%;
+        background:aqua;
+        position: absolute;
+        top: -14px;
+        left: 0px;
+      }
+      .an{
+        position: absolute;
+        top: 20px;
+        left: -30px;
+      }
+      .liang{
+        position: absolute;
+        top: 20px;
+        right: -30px;;
+      }
+    }
+
   }
 .animation {
   background: url("~@lib/@{imgPath}/btn_ac_mode_e@2x.png") no-repeat;
@@ -500,6 +629,28 @@ export default {
         border-color: #ffbf00;
       }
     }
+  }
+
+
+}
+.ratiobg111{
+  position: fixed;
+  right: 100px;
+  top: 200px;
+  height: 500px;
+  width: 10px;
+  background: #ccc;
+  .circle{
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    background: #fff;
+    position: absolute;
+    left: -15px;
+  }
+  .ratiobg2{
+    // border-radius: 50%;
+    background: rgba(255 ,255, 255, 0.6)
   }
 }
 </style>
