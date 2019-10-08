923f91a4f18085ce599e72c1349ec277639effde
chemeixia <694712153@qq.com>
2019-10-08 17:00:54 +0800

    修复博联空气云管家展示盒子位置问题

--- src/broadLink/air_quality_check_m/components/Index.vue
+++ src/broadLink/air_quality_check_m/components/Index.vue
@@ -201,7 +201,9 @@ i {
   }
   .bgc {
     position: absolute;
-    transform: translate(0,0);
+    transform: translate(-50%,-50%);
+    left: 50%;
+    top: 50%;
     width: 420px;
     height: 420px;
     border-radius: 50%;
