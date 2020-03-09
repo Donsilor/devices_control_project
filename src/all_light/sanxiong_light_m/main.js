f1853ecb82f012d60b4684b6d02ed03e9a42a54b
Hykleri <hykleri@aliyun.com>
2020-03-09 15:46:23 +0800

    dev：鸿雁三雄灯新UI开发完成

--- src/all_light/sanxiong_light_m/main.js
+++ src/all_light/sanxiong_light_m/main.js
@@ -9,14 +9,35 @@ import '@lib/base/reset.less'
 import '@lib/base/common.less'
 
 import store from '@lib/store/index.js'
-// import Topbar from '@lib/components/Topbar.vue'
 import NewTopbar from '@lib/components/NewTopBar.vue'
 import StatusTip from '@lib/components/StatusTip.vue'
+
+// H5路由处理
+import Router from 'vue-router'
+Vue.use(Router)
+import Index from './Index.vue'
+import OfflineHelpPage from '@lib/components/OfflineHelpPage.vue'
+
+
+const router = new Router({
+  routes: [{
+    path: '/',
+    name: 'index',
+    component: Index,
+  },
+  {
+    path: '/OfflineHelpPage',
+    name: 'OfflineHelpPage',
+    component: OfflineHelpPage,
+  }
+  ]
+})
 
 Vue.component('new-topbar', NewTopbar)
 Vue.component('StatusTip', StatusTip)
 new Vue({
     el: '#app',
     store,
+    router,
     render: h => h(App)
 })
